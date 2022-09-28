f601a59a6e19a1f77a5c5f4780a7068efb5c52d6
Anthony Whipple <whipple@email.unc.edu>
2022-09-28 13:44:43 -0400

    Added debugging information to log file

--- process_pressure.py
+++ process_pressure.py
@@ -2,7 +2,7 @@ import os
 from unicodedata import numeric
 from pytz import timezone
 import requests
-import datetime
+from datetime import datetime
 import pandas as pd
 from io import StringIO
 from urllib.request import urlopen
@@ -10,10 +10,18 @@ import xmltodict
 import numpy as np
 import warnings
 from sqlalchemy import create_engine
+import inspect
 
 ########################
 # Utility functions    #
 ########################
+
+# override print so each statement is timestamped
+old_print = print
+def timestamped_print(*args, **kwargs):
+  old_print(datetime.now(), *args, **kwargs)
+print = timestamped_print
+
 
 def slicer(my_str,sub):
         index=my_str.find(sub)
@@ -63,6 +71,7 @@ def get_noaa_atm(id, begin_date, end_date):
     Returns:
         r_df (pd.DataFrame): DataFrame of atmospheric pressure from specified station and time range. Dates in UTC
     """    
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
     
     query = {'station' : str(id),
              'begin_date' : begin_date,
@@ -96,6 +105,7 @@ def get_nws_atm(id, begin_date, end_date):
     Returns:
         response (str): Still working on this!        
     """    
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
     
     new_begin_date = pd.to_datetime(begin_date, utc=True) - datetime.timedelta(seconds = 3600)
     new_end_date = pd.to_datetime(end_date, utc=True) + datetime.timedelta(seconds = 3600)
@@ -123,6 +133,7 @@ def get_isu_atm(id, begin_date, end_date):
         begin_date (str): Beginning date of requested time period. Format: %Y%m%d %H:%M
         end_date (str): End date of requested time period. Format: %Y%m%d %H:%M
     """   
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
     
     new_begin_date = pd.to_datetime(begin_date, utc=True) 
     new_end_date = pd.to_datetime(end_date, utc=True) + datetime.timedelta(days=1)
@@ -164,6 +175,7 @@ def get_fiman_atm(id, begin_date, end_date):
     Returns:
         r_df (pd.DataFrame): DataFrame of atmospheric pressure from specified station and time range. Dates in UTC
     """    
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
     
     fiman_gauge_keys = pd.read_csv("data/fiman_gauge_key.csv").query("site_id == @id & Sensor == 'Barometric Pressure'")
     
@@ -211,6 +223,8 @@ def get_atm_pressure(atm_id, atm_src, begin_date, end_date):
     Returns:
         pandas.DataFrame: Atmospheric pressure data for the specified time range and source
     """    
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
+
     match atm_src.upper():
         case "NOAA":
             return get_noaa_atm(id = atm_id, begin_date = begin_date, end_date = end_date)
@@ -225,11 +239,14 @@ def get_atm_pressure(atm_id, atm_src, begin_date, end_date):
         
         
 def interpolate_atm_data(x, debug = True):
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
     place_names = list(x["place"].unique())
     
     interpolated_data = pd.DataFrame()
     
     for selected_place in place_names:
+        print("for " + selected_place)
+        
         selected_data = x.query("place == @selected_place").copy()
         selected_data["pressure_mb"] = np.nan
         
@@ -260,8 +277,7 @@ def interpolate_atm_data(x, debug = True):
                                             begin_date = dt_min.strftime("%Y%m%d %H:%M"),
                                             end_date = dt_max.strftime("%Y%m%d %H:%M")).drop_duplicates()     
             
-        if(atm_data.empty):
-            
+        if(atm_data.empty):            
             warnings.warn(message = f"No atm pressure data available for: {selected_place}")
             pass
                         
@@ -282,6 +298,8 @@ def interpolate_atm_data(x, debug = True):
 
 
 def match_measurements_to_survey(measurements, surveys):
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
+
     sites = measurements["sensor_ID"].unique()
     survey_sites = surveys["sensor_ID"].unique()
     
@@ -294,6 +312,8 @@ def match_measurements_to_survey(measurements, surveys):
     matched_measurements = pd.DataFrame()
     
     for selected_site in matching_sites:
+        print(selected_site)
+        
         selected_measurements = measurements.query("sensor_ID == @selected_site").copy()
         
         selected_survey = surveys.query("sensor_ID == @selected_site")
@@ -311,7 +331,7 @@ def match_measurements_to_survey(measurements, surveys):
             selected_measurements["date_surveyed"] = pd.to_datetime(np.where(selected_measurements["date"] >= survey_dates[0], survey_dates[0], np.nan), utc = True)
             
         if number_of_surveys > 1:
-            survey_dates.append(pd.to_datetime(datetime.datetime.utcnow(), utc=True))
+            survey_dates.append(pd.to_datetime(datetime.utcnow(), utc=True))
             selected_measurements["date_surveyed"] = pd.to_datetime(pd.cut(selected_measurements["date"], bins = survey_dates, labels = survey_dates[:-1]), utc = True)
     
         merged_measurements_and_surveys = pd.merge(selected_measurements, surveys, how = "left", on = ["place","sensor_ID","date_surveyed"])
@@ -319,11 +339,13 @@ def match_measurements_to_survey(measurements, surveys):
         matched_measurements = pd.concat([matched_measurements, merged_measurements_and_surveys]).drop_duplicates()
         matched_measurements["notes"] = matched_measurements["notes_x"]
         matched_measurements.drop(columns = ['notes_x','notes_y'],inplace=True)
+        print("matched_measurements.shape: ", matched_measurements.shape)
         
     return matched_measurements
 
 
 def format_interpolated_data(x):
+    print(inspect.stack()[0][3])    # print the name of the function we just entered
 
     formatted_data = x.copy()
     formatted_data.rename(columns = {"pressure_mb":'atm_pressure', 'pressure':'sensor_pressure'}, inplace = True)
@@ -340,6 +362,7 @@ def format_interpolated_data(x):
 
 
 def main():
+    print("Entering main of process_pressure.py")
     
     # from env_vars import set_env_vars
     # set_env_vars()
